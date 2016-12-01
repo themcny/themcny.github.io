0157505309da98bc6327d91e9b9c07d7099774c3
Nicole Yee <n.lindsey.yee@gmail.com>
2016-11-30 18:04:41 -0800

    Delete old files

--- blog/extras/cycle.rb
+++
@@ -1,5 +0,0 @@
-array = [1,2,3]
-array.cycle(3) { |x| puts x }
-
-strings = ['a','b','c']
-strings.cycle(2) { |x| puts x*4 }
--- blog/extras/fib.rb
+++
@@ -1,9 +0,0 @@
-# Returns the nth number of the Fibonacci Sequence
-
-def fib(n)
-    if n < 2
-        return 1
-    else
-        return fib(n-2) + fib(n-1)
-    end
-end
